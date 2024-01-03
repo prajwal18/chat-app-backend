fcb5a4d6624f5a964b6041df036aa14be5638865
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-01-03 10:36:29 +0545

    fix: fixed message serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,11 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :message
-  belongs_to :sender
-  belongs_to :receiver
+  attributes :id, :message, :sender, :receiver
+
+  def sender
+    JSON.parse object.sender.to_json(only: %i[id name]).to_s
+  end
+
+  def receiver
+    JSON.parse object.receiver.to_json(only: %i[id name]).to_s
+  end
 end
